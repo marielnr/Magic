f99f3f349e3cc810f68cd43368786e6bb6bd90cb
Mariel Nava Rodríguez <marielnava.rdz@outlook.com>
2025-07-28 09:54:09 +0200

    Primer commit del proyecto POwer BI

---
+++ .anaconda/navigator/scripts/anaconda_prompt.bat
@@ -0,0 +1,4 @@
+@echo off
+chcp 1252
+call C:\Users\marie\anaconda3\Scripts\activate C:\Users\marie\anaconda3
+start cmd.exe \K "C:\Users\marie\anaconda3\Scripts\activate.bat" "C:\Users\marie\anaconda3" >C:\Users\marie\AppData\Roaming\SPB_Data\.anaconda\navigator\scripts\anaconda_prompt-out-1.txt 2>C:\Users\marie\AppData\Roaming\SPB_Data\.anaconda\navigator\scripts\anaconda_prompt-err-1.txt
---
+++ .anaconda/navigator/scripts/spyder.bat
@@ -0,0 +1,4 @@
+@echo off
+chcp 1252
+call C:\Users\marie\anaconda3\Scripts\activate C:\Users\marie\anaconda3
+spyder >C:\Users\marie\AppData\Roaming\SPB_Data\.anaconda\navigator\scripts\spyder-out-1.txt 2>C:\Users\marie\AppData\Roaming\SPB_Data\.anaconda\navigator\scripts\spyder-err-1.txt
