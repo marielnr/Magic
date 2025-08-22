f0b5abb3746ee3d72ed86d6e492ebbe90604120e
Mariel Nava Rodríguez <marielnava.rdz@outlook.com>
2025-08-22 13:38:11 +0200

    Delete .anaconda/navigator directory

--- .anaconda/navigator/scripts/anaconda-navigator-updater.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-chcp 1252
-call C:\Users\marie\anaconda3\Scripts\activate C:\Users\marie\anaconda3
-navigator-updater --latest-version 2.6.4 --prefix C:\Users\marie\anaconda3 >C:\Users\marie\AppData\Roaming\SPB_Data\.anaconda\navigator\scripts\anaconda-navigator-updater-out-1.txt 2>C:\Users\marie\AppData\Roaming\SPB_Data\.anaconda\navigator\scripts\anaconda-navigator-updater-err-1.txt
--- .anaconda/navigator/scripts/spyder.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-chcp 1252
-call C:\Users\marie\anaconda3\Scripts\activate C:\Users\marie\anaconda3
-spyder >C:\Users\marie\AppData\Roaming\SPB_Data\.anaconda\navigator\scripts\spyder-out-1.txt 2>C:\Users\marie\AppData\Roaming\SPB_Data\.anaconda\navigator\scripts\spyder-err-1.txt
